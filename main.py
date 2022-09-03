0a713be66de4e9a7a46491164bc0c452b81d4144
meow <abc@gmail.com>
2022-09-03 23:43:43 +0300

    pick-words

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-word_bank = {'love': {'noun': ['love', 'partner', 'se'], 
+import random
+
+word_bank = {'love': {'noun': ['love', 'partner', 'sex'], 
                       'verb': ['is', 'are'],
                       'adjective': ['good', 'bad', 'awesome'],
                       },
@@ -15,16 +17,32 @@ word_bank = {'love': {'noun': ['love', 'partner', 'se'],
 def pick_topic(d):
     print('CHOOSE A TOPIC: ')
     topics = []
+    choice_d = {}
     for key in d.keys():
         topics.append(key.upper())
     for index, topic in enumerate(topics):
         print(index, topic)
-    
-    choice = input('YOUR CHOICE: ')
-    return int(choice)
+        choice_d[index] = topic    
+    choice = int(input('YOUR CHOICE: '))
+    choice = choice_d[choice]
+    return choice
 
-def pick_noun(d):
-    pass
+def pick_words(d, topic):
+    l = []
+    keys = ()
+    for key in d[topic.lower()].keys():
+        keys.add(key)
+
+    counter = 0
+    while counter <= 3:
+        l.append(random.choice(d[topic.lower()][keys[0]]))
+        counter += 1
+    print(l)
+
+pick_words(word_bank, pick_topic(word_bank))
+
+# print(pick_words(word_bank, pick_topic(word_bank)))  
+          
 
 def pick_verb(d):
     pass
